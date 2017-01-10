566f1cc7edfeaf8595c041213b63a52fac0a1582
当轩 <code.falling@gmail.com>
2017-01-10 23:55:16 +0800

    fix unicode, thanks to @binblog

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ hexo.extend.filter.register('after_post_render', function(data){
     var toprocess = ['excerpt', 'more', 'content'];
     for(var i = 0; i < toprocess.length; i++){
       var key = toprocess[i];
-
+ 
       var $ = cheerio.load(data[key], {
         ignoreWhitespace: false,
         xmlMode: false,
-        lowerCaseTags: false
+        lowerCaseTags: false,
+        decodeEntities: false
       });
 
       $('img').each(function(){
